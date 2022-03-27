6d09f8329bc29b5648aa6d9c0da97db81db84cf5
yeontory <yjj0287@gmail.com>
2022-03-27 17:32:40 +0900

    Code refactoring

---
+++ src/main.js
@@ -0,0 +1,35 @@
+'use strict';
+import PopUp from "./popup.js";
+import { GameBuilder, Reason } from "./game.js";
+import * as sound from './sound.js';
+
+const gameFinishBanner = new PopUp();
+const game = new GameBuilder()
+    .withGameDuration(5)
+    .withCarrotCount(3)
+    .withBugCount(3)
+    .build();
+
+game.setGameStopListener(reason=> {
+    let message;
+    switch(reason) {
+        case Reason.cancel:
+            message = 'Replay❓';
+            sound.playAlert();
+            break;
+        case Reason.win:
+            message = 'YOU WON 🎉';
+            sound.playWin();
+            break;
+        case Reason.lose:
+            message = 'YOU LOST💩';
+            sound.playBug();
+            break;
+        default:
+            throw new Error('not valid reason');
+    }
+    gameFinishBanner.showWithText(message);
+});
+gameFinishBanner.setClickListener(()=> {
+    game.start();
+});
